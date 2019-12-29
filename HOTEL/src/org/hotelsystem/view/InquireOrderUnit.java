7a3145627a530f288f041cfda1d7facd417c41b3
jasoonn <b05901171@ntu.edu.tw>
2019-12-29 13:50:05 +0800

    add InquireUI

---
+++ HOTEL/src/org/hotelsystem/view/InquireOrderUnit.java
@@ -0,0 +1,82 @@
+package org.hotelsystem.view;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class InquireOrderUnit extends JPanel implements ActionListener{
+    private JLabel labelHotelImage;
+    private JLabel labelHotelName;
+    private JLabel labelRoomType;
+    private JLabel checkInAndOut;
+    private JLabel labelOrderID;
+    private JLabel labelPrice;
+    public JButton btnReserve;
+
+    public InquireOrderUnit() {
+        initUI();
+    }
+
+    private void initUI() {
+        LineBorder line = new LineBorder(Color.GRAY);
+        EmptyBorder empty = new EmptyBorder(5, 5, 5, 5);
+        this.setBorder(new CompoundBorder(line, empty));
+        this.setLayout(new GridBagLayout());
+
+        this.labelHotelImage = new JLabel("Hotel Image");
+        this.labelHotelImage.setHorizontalAlignment(JLabel.CENTER);
+        this.labelHotelImage.setBorder(new LineBorder(Color.BLACK));
+        this.addWithConstraints(labelHotelImage, 0, 0, 4, 4, 4, 4,
+            GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+
+        this.labelHotelName = new JLabel("Hotel Name: ");
+        this.addWithConstraints(labelHotelName, 8, 1, 4, 1, 4, 1,
+            GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+
+        this.labelRoomType = new JLabel("Rooms: ");
+        this.addWithConstraints(labelRoomType, 8, 3, 8, 1, 8, 1,
+            GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+
+        this.checkInAndOut = new JLabel("Check in/out: ");
+        this.addWithConstraints(checkInAndOut, 8, 2, 8, 1, 8, 1,
+            GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+
+
+        this.labelOrderID = new JLabel("Order ID: ");
+        this.addWithConstraints(labelOrderID, 4, 0, 4, 1, 4, 1,
+            GridBagConstraints.NONE, GridBagConstraints.CENTER);
+
+
+        this.labelPrice = new JLabel("NTD 168,000");
+        this.addWithConstraints(labelPrice, 12, 1, 4, 1, 4, 1,
+            GridBagConstraints.NONE, GridBagConstraints.EAST);
+
+        this.btnReserve = new JButton("Show Order!");
+        this.btnReserve.addActionListener(this);
+        this.addWithConstraints(btnReserve, 12, 3, 4, 1, 4, 1,
+            GridBagConstraints.NONE, GridBagConstraints.EAST);
+    }
+
+    private void addWithConstraints(JComponent c, int gridx, int gridy,
+			int gridwidth, int gridheight, int weightx, int weighty,
+			int fill, int anchor) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.gridwidth = gridwidth;
+		gbc.gridheight = gridheight;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		gbc.fill = fill;
+		gbc.anchor = anchor;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		this.add(c, gbc);
+    }
+
+    public void actionPerformed(ActionEvent e){  
+        if( e.getSource() == this.btnReserve){
+            System.out.println("Show order triggered.");
+        }
+    }  
+}
--- HOTEL/src/org/hotelsystem/view/InquireUI.java
+++ HOTEL/src/org/hotelsystem/view/InquireUI.java
@@ -3,37 +3,89 @@ package org.hotelsystem.view;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
+import java.awt.event.*;
 
-public class InquireUI extends JPanel{
-    private JPanel userInfo;
-    private JPanel seachOrder;
-    private JPanel showGeneralOrder;
+public class InquireUI extends JPanel implements ActionListener{
+  private JPanel userInfo;
+  private JPanel searchOrder;
+  private JPanel showGeneralOrder;
+  private JButton searchOrderButton;
 
-    public InquireUI() {
-		this.setLayout(new GridBagLayout());
-		initUI();
-    }
+  private String userName;
+  private int orderSum;
+  private int[] orderIDs;
+  
+  public InquireUI() {
+    this.setLayout(new GridBagLayout());
+    initUI();
+  }
     
-    private void initUI() {
-        this.userInfo = new JPanel();
-        this.userInfo.setBorder(new LineBorder(Color.RED));
-        this.addWithConstraints(this.userInfo, 0, 0, 1, 2, 2, 1,
-                                GridBagConstraints.BOTH, GridBagConstraints.WEST);
+  private void initUI() {
+    this.userName = "Default";
+    this.orderSum = 10;
+    int[] buf = {1,2,3,4,5};
+    this.orderIDs = buf;
 
-        this.seachOrder = new JPanel();
-        this.seachOrder.setBorder(new LineBorder(Color.GREEN));
-        this.addWithConstraints(this.seachOrder, 0, 2, 1, 3, 2, 2,
-                                GridBagConstraints.BOTH, GridBagConstraints.WEST);
+    LineBorder line = new LineBorder(Color.BLACK);
+    EmptyBorder empty = new EmptyBorder(10, 10, 10, 10);
 
-        this.showGeneralOrder = new JPanel();
-        this.showGeneralOrder.setBorder(new LineBorder(Color.BLUE));
-        this.addWithConstraints(this.showGeneralOrder, 1, 0, 4, 5, 7, 2,
-                                GridBagConstraints.BOTH, GridBagConstraints.WEST);
+    //Search Order Part
+    this.searchOrder = new JPanel();
+    this.searchOrder.setBorder(new CompoundBorder(line, new EmptyBorder(10, 20, 10, 20)));
+    this.searchOrder.setLayout(new GridBagLayout());
+    this.addWithConstraints(this.searchOrder, 0, 0, 1, 2, 2, 1,
+        GridBagConstraints.BOTH, GridBagConstraints.CENTER);
 
+    JLabel searchOrderLabel = new JLabel("PLease input order ID! ");
+    searchOrderLabel.setHorizontalAlignment(JLabel.CENTER);
+    this.addWithConstraints(this.searchOrder, searchOrderLabel, 0, 0, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.SOUTH);
+        
+    JTextField searchOrderTextField = new JTextField();
+    addWithConstraints(this.searchOrder, searchOrderTextField, 0, 1, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
 
-    }
+    this.searchOrderButton = new JButton("Send query!");
+    this.searchOrderButton.addActionListener(this);
+    this.addWithConstraints(this.searchOrder, this.searchOrderButton, 0, 2, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.NORTH);
 
-    private void addWithConstraints(JComponent c, int gridx, int gridy,
+    //User Overview Part
+    this.userInfo = new JPanel();
+    this.userInfo.setBorder(new CompoundBorder(line, empty));
+    this.userInfo.setLayout(new GridBagLayout());
+    this.addWithConstraints(this.userInfo, 0, 2, 1, 3, 2, 2,
+        GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        
+    JLabel userOverview = new JLabel("User Overview");
+    userOverview.setHorizontalAlignment(JLabel.CENTER);
+    this.addWithConstraints(this.userInfo, userOverview, 0, 0, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+
+    JLabel userNameLabel = new JLabel(" User Name: " + userName);
+    this.addWithConstraints(this.userInfo, userNameLabel, 0, 1, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.NORTH);
+        
+    JLabel orderSumLabel = new JLabel(" Total Order: " + orderSum);
+    this.addWithConstraints(this.userInfo, orderSumLabel, 0, 2, 1, 1, 1, 1,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.NORTH);
+
+    JLabel orderQuickView = new JLabel(" Order ID: " + orderIDsToString());
+    this.addWithConstraints(this.userInfo, orderQuickView, 0, 3, 1, 1, 1, 2,
+        GridBagConstraints.HORIZONTAL, GridBagConstraints.NORTH);
+        
+
+    
+    
+    this.showGeneralOrder = new InquireOrders();
+    this.showGeneralOrder.setBorder(new CompoundBorder(line, empty));
+    this.addWithConstraints(this.showGeneralOrder, 1, 0, 4, 5, 30, 2,
+        GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+    
+  }
+    
+
+  private void addWithConstraints(JComponent c, int gridx, int gridy,
 			int gridwidth, int gridheight, int weightx, int weighty,
 			int fill, int anchor) {
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -47,6 +99,37 @@ public class InquireUI extends JPanel{
 		gbc.anchor = anchor;
 		gbc.insets = new Insets(3, 3, 3, 3);
 		this.add(c, gbc);
-	}
+  }
+
+  private void addWithConstraints(JPanel p, JComponent c,
+        int gridx, int gridy, int gridwidth, int gridheight,
+        int weightx, int weighty, int fill, int anchor) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.gridwidth = gridwidth;
+		gbc.gridheight = gridheight;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		gbc.fill = fill;
+		gbc.anchor = anchor;
+		// gbc.insets = new Insets(5, 5, 5, 5);
+		p.add(c, gbc);
+  }
+
+  private String orderIDsToString(){
+    String buf = "";
+    for (int i =0; i<this.orderIDs.length;i++){
+      buf = buf + this.orderIDs[i];
+      if (i!=this.orderIDs.length-1) buf = buf + ", "; 
+    }
+    return buf;
+  }
+
+  public void actionPerformed(ActionEvent e){  
+    if( e.getSource() == this.searchOrderButton){
+        System.out.println("Search triggered.");
+    }
+  }  
 	
 }
