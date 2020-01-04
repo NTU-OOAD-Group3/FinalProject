57f5a6b61a2c9684af065d8d9ff90e9af8b32bbd
Riino <sorphwer@gmail.com>
2020-01-04 13:56:12 +0800

    update loginControl

--- HOTEL/src/main.java
+++ HOTEL/src/main.java
@@ -16,6 +16,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 public class main {
     public static void main(String[] args) {
-        
+        //via DB:
+       
+        //LoginStatus status =new LoginStatus();//MODEL
+        //LoginControl loginControl = new LoginControl(status);//CONTROL with MODEL
+        //MainFrame mainFrame = new MainFrame(loginControl,xxxControl,...);//VIEW with CONTROL
+       // loginControl.setView(mainFrame.getLoginUI());//ADD VIEW INTO CONTROL
     }
 }
